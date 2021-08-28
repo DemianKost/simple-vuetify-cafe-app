b09c59d29ce34c5d1d6459e6958a14ae0d3e9cca
DemianWHT <demiankost2@gmail.com>
2021-08-28 18:02:55 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <Header></Header>
+
+    <v-main>
+      <v-container class="pt-5">
+        <router-view class="view"></router-view>
+      </v-container>
+    </v-main>
+
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+    Footer,
+  },
+
+  data: () => ({
+    drawer: false,
+    group: ''
+  }),
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Cafe from '../components/Cafe.vue'
+import CafeView from '../components/CafeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Cafe',
+    component: Cafe
+  },
+  {
+    path: '/cafe/view',
+    name: 'CafeView',
+    component: CafeView
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
